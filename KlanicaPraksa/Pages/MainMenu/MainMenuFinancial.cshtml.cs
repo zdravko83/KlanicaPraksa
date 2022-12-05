b35ead641bf8aa5cbe5aa3149cb27132c2703707
Cxpe97 <darkusmaxazero2@gmail.com>
2022-12-05 14:23:28 +0100

    Izmene Petar

---
+++ KlanicaPraksa/Pages/MainMenu/MainMenuFinancial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.MainMenu
+{
+    public class MainMenuFinancialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
