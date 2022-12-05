2552cad586cf6bc2b62f946c3060a96c697a15ee
Ljubomir Tomic <tomic.ljuba01@gmail.com>
2022-12-05 15:35:06 +0100

    Ljubomir Tomic treba izmeniti finansije predzadnje

---
+++ KlanicaPraksa/Pages/MainMenu/MaimMenuFinanicial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.MainMenu
+{
+    public class MaimMenuFinanicialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
