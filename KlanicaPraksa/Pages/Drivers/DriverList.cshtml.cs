c8e5f3e99757c3475c19362eaa0de51adc7c448e
Cxpe97 <darkusmaxazero2@gmail.com>
2022-12-06 15:26:41 +0100

    Petar Maksimovic - Driver - nije uradjen popup is-active na klik

---
+++ KlanicaPraksa/Pages/Drivers/DriverList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.Drivers
+{
+    public class DriverListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
