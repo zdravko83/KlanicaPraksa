d4c7e83ec9b74cc86f7f1453fa260ae27e0217c6
Ljubomir Tomic <tomic.ljuba01@gmail.com>
2022-12-06 14:32:18 +0100

    tomic ljubomir favorlist

---
+++ KlanicaPraksa/Pages/Products/FavorAddEdit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.Products
+{
+    public class FavorAddEditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
