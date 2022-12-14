ffdd0ac1f40d32502fdab17d190f8242ea711b83
Cxpe97 <darkusmaxazero2@gmail.com>
2022-12-14 14:29:39 +0100

    Petar - Refinement_Create, Refinement_Input_Barcode, Refinement_Input_Libela

---
+++ KlanicaPraksa/Pages/Refinements/Refinement_Input_Libela.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.Refinements
+{
+    public class Refinement_Input_LibelaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
