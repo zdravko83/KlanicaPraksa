5d51b013023c3352d5bcf9097f514120e25abe72
Zdravko Ivankovic <ivankovic.zdravko@gmail.com>
2022-12-20 08:18:44 +0100

    Dodatne izmene

---
+++ KlanicaPraksa/Pages/FinalProducts/FinalProduct-Initial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.FinalProducts
+{
+    public class FinalProduct_InitialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KlanicaPraksa/Pages/FinalProducts/FinalProduct-Measuring.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KlanicaPraksa.Pages.FinalProducts
+{
+    public class FinalProduct_MeasuringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
